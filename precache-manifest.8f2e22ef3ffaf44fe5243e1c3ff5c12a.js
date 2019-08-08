c3362b3e859c1170c5d292783f9cc0a573ceb874
Moody <moody@Moodys-MacBook-Pro.local>
2019-08-07 22:30:53 -0500

    github pages

---
+++ precache-manifest.8f2e22ef3ffaf44fe5243e1c3ff5c12a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7186f290e0f50a5de50906bee4c6a0e6",
+    "url": "/smtgames/index.html"
+  },
+  {
+    "revision": "511b833999a44f374283",
+    "url": "/smtgames/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/smtgames/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "511b833999a44f374283",
+    "url": "/smtgames/static/js/main.3c124345.chunk.js"
+  },
+  {
+    "revision": "01708b0ece58f8b39160",
+    "url": "/smtgames/static/js/runtime~main.af5fde56.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/smtgames/static/media/logo.5d5d9eef.svg"
+  }
+]);
